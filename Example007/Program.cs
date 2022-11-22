6a683f8561d523994eb975fbb3a1c1f98909fed2
a.gazalov <gazalov@list.ru>
2022-11-22 20:21:13 +0300

    Code max improved

--- Example007/Program.cs
+++ Example007/Program.cs
@@ -10,14 +10,16 @@ int a1 = 15;
 int b1 = 21;
 int c1 = 39;
 int a2 = 12;
-int b2 = 23;
+int b2 = 243;
 int c2 = 33;
 int a3 = 13;
 int b3 = 63;
 int c3 = 33;
 
-int max1 = Max(a1, b1, c1);
-int max2 = Max(a2, b2, c2);
-int max3 = Max(a3, b3, c3);
-int max = Max(max1, max2, max3);
+// int max1 = Max(a1, b1, c1);
+// int max2 = Max(a2, b2, c2);
+// int max3 = Max(a3, b3, c3);
+// int max = Max(max1, max2, max3);
+
+int max = Max(Max(a1, b1, c1), Max(a2, b2, c2), Max(a3, b3, c3));
 Console.WriteLine(max);
